265f8f9cd2f955a7513b7be42d3bb55644859b16
newblue911 <162079072+newblue911@users.noreply.github.com>
2024-03-04 11:50:40 +0800

    Update diy-part2.sh-openclash

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -34,6 +34,10 @@ sed -i 's|TARGET_CFLAGS += -DHAVE_MAP_SYNC.*|TARGET_CFLAGS += -DHAVE_MAP_SYNC $(
 # Add luci-app-amlogic
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+
+# 20240304
+git clone https://github.com/vernesong/OpenClash.git package/openclash 
+
 #
 # Apply patch
 # git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
